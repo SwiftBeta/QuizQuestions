87e79cf02599f56a33ceb5aa5071aadcaca7e104
SwiftBeta <swiftbeta.blog@gmail.com>
2022-04-18 10:20:12 +0200

    Update script

--- .github/prepare_new_week_script.sh
+++ .github/prepare_new_week_script.sh
@@ -15,18 +15,20 @@ mv next.json current.json
 # Copy template to start adding new question for the next week
 cp template.json next.json
 
-# Remove last line
-sed -i '' -e '$ d' archive/weeks.json
-# Remove last line
-sed -i '' -e '$ d' archive/weeks.json
+# Remove last line macos
+#sed -i '' -e '$ d' archive/weeks.json
+# Remove last line ubuntu
+sed '$d' archive/weeks.json
+sed '$d' archive/weeks.json
+
 # Add new line
 echo '\t\t\t"week$NEW_WEEK_NUMBER.json",\n\t]\n}' >> archive/weeks.json
 
 git config --global user.email "swiftbeta.blog@gmail.com"
 git config --global user.name "SwiftBeta"
 
-git add .
-git commit -m "🤖 Week "$NEW_WEEK_NUMBER", Update questions"
-git push
+# git add .
+# git commit -m "🤖 Week "$NEW_WEEK_NUMBER", Update questions"
+# git push
 
 echo "Finished script..."
