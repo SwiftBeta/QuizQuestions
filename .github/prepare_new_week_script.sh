d5e98941dce21c3f547fc0944963af1a60a34c7f
SwiftBeta <74316958+SwiftBeta@users.noreply.github.com>
2022-05-02 15:22:55 +0200

    Update prepare_new_week_script.sh

--- .github/prepare_new_week_script.sh
+++ .github/prepare_new_week_script.sh
@@ -3,32 +3,32 @@
 echo "Executing script..."
 
 # Extract new filename
-LAST_FILENAME=$(ls archive | sort -Vf | tail -n 1)
-LAST_WEEK_NUMBER=$(echo ${LAST_FILENAME} | grep -o -E "[0-9]+")
-NEW_WEEK_NUMBER=$(echo $(($LAST_WEEK_NUMBER + 2)))
-echo $NEW_WEEK_NUMBER
+#LAST_FILENAME=$(ls archive | sort -Vf | tail -n 1)
+#LAST_WEEK_NUMBER=$(echo ${LAST_FILENAME} | grep -o -E "[0-9]+")
+#NEW_WEEK_NUMBER=$(echo $(($LAST_WEEK_NUMBER + 2)))
+#echo $NEW_WEEK_NUMBER
 
 # Archive current week
-mv current.json archive/week$NEW_WEEK_NUMBER.json
+#mv current.json archive/week$NEW_WEEK_NUMBER.json
 # Move new questions to current. These questions will be displayed during the week
-mv next.json current.json
+#mv next.json current.json
 # Copy template to start adding new question for the next week
-cp template.json next.json
+#cp template.json next.json
 
 # Remove last line macos
 #sed -i '' -e '$ d' archive/weeks.json
 # Remove last line ubuntu
-sed '$d' archive/weeks.json
-sed '$d' archive/weeks.json
+#sed '$d' archive/weeks.json
+#sed '$d' archive/weeks.json
 
 # Add new line
-echo '\t\t\t"week$NEW_WEEK_NUMBER.json",\n\t]\n}' >> archive/weeks.json
+#echo '\t\t\t"week$NEW_WEEK_NUMBER.json",\n\t]\n}' >> archive/weeks.json
 
-git config --global user.email "swiftbeta.blog@gmail.com"
-git config --global user.name "SwiftBeta"
+#git config --global user.email "swiftbeta.blog@gmail.com"
+#git config --global user.name "SwiftBeta"
 
-git add .
-git commit -m "🤖 Week "$NEW_WEEK_NUMBER", Update questions"
-git push
+#git add .
+#git commit -m "🤖 Week "$NEW_WEEK_NUMBER", Update questions"
+#git push
 
-echo "Finished script..."
+#echo "Finished script..."
